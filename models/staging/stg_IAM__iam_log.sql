09f7d5be6e951bcefbc56a20cd7acfbccf5ae7ca
davide.scarra <davide.scarra@ext.randstad.it>
2025-07-10 12:37:27 +0200

    refactor: standardize column names and descriptions in staging models for clarity and consistency

--- models/staging/stg_IAM__iam_log.sql
+++ models/staging/stg_IAM__iam_log.sql
@@ -1,8 +1,9 @@
 -- models/staging/stg_IAM__iam_log.sql
 
 select
-    IdFile,
-    InsertDate,
-    Period
+    -- {{ dbt_utils.generate_surrogate_key(['IdFile', 'Period']) }} as iam_log_id, chiave surrogata
+    IdFile as id_file,
+    InsertDate as insert_date,
+    Period as period
 
 from {{ source('iam_log', 'IAM_LOG') }}
